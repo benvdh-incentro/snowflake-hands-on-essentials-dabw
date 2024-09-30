44632ccd3eda42249dc7cb1bf65582fec3ab3bb2
Ben van der Harg <ben.vanderharg@incentro.com>
2024-09-30 16:17:36 -0400

    Add fruityvice information (static version)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("Customize your Smoothie! :cup_with_straw:")
@@ -41,3 +42,7 @@ if ingredients:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+
+# New section to display fruityvice nutrition information
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
